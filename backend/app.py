b51ce8167f23e86960335c0fef10aab1d3deec24
Suhaib Sheriff <mightydrummer7@gmail.com>
2026-02-06 12:08:31 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -161,3 +161,7 @@ def download_report():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
