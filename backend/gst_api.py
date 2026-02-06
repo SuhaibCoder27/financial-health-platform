d6fae59b512af99504e20dd6b5f7f63216a14b6b
Suhaib Sheriff <mightydrummer7@gmail.com>
2026-02-06 11:52:05 +0530

    Create gst_api.py

---
+++ backend/gst_api.py
@@ -0,0 +1,7 @@
+def fetch_gst_data():
+    return {
+        "gst_number": "29ABCDE1234F1Z5",
+        "last_filed_period": "Dec 2025",
+        "tax_paid": 180000,
+        "gst_status": "Filed"
+    }
